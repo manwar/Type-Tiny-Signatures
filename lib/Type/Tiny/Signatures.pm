390a30cc42b07c1fafd65f1f63190aee38c616d6
Al Newkirk <anewkirk@ana.io>
2015-06-14 19:55:36 -0400

    Minor updates to code and POD.

--- lib/Type/Tiny/Signatures.pm
+++ lib/Type/Tiny/Signatures.pm
@@ -1,4 +1,4 @@
-# ABSTRACT: Method/Function Signatures using Type::Tiny Constraints
+# ABSTRACT: Method/Function Signatures w/Type::Tiny Constraints
 package Type::Tiny::Signatures;
 
 use 5.14.0;
@@ -8,7 +8,6 @@ use warnings;
 require Function::Parameters;
 require Type::Registry;
 require Type::Tiny;
-
 
 our $CALLER = caller;
 our @CONFIG = 'Types::Standard';
@@ -20,9 +19,8 @@ unshift @Function::Parameters::type_reifiers => sub {
 };
 
 sub import {
-    my ($class, @libraries) = @_;
-    @libraries = map { ref $_ ? @$_ : $_ } @libraries;
-    Type::Registry->for_class($CALLER)->add_types($_) for @CONFIG, @libraries;
+    my @LIBRARIES = map { ref $_ ? @$_ : $_ } splice @_, 1;
+    Type::Registry->for_class($CALLER)->add_types($_) for @CONFIG, @LIBRARIES;
     Function::Parameters->import;
 }
 
@@ -41,8 +39,8 @@ sub import {
 
 =head1 DESCRIPTION
 
-This module uses L<Function::Parameters> to extends Perl with keywords that let
-you define methods and functions with parameter lists which can be validated
+This module uses L<Function::Parameters> to extends Perl with keywords that
+let you define methods and functions with parameter lists which can be validated
 using L<Type::Tiny> type constraints. The type constraints can be provided by
 the Type::Tiny standard library, L<Types::Standard>, or any supported
 user-defined type library which can be a L<Moose>, L<MooseX::Type>,
@@ -54,7 +52,8 @@ L<MouseX::Type>, or L<Type::Library> library.
 The method and function signatures can be configured to incorporate the
 user-defined type constraints in routine parameter list validation by passing
 the library package name as an argument to the Type::Tiny::Signatures usage
-declaration.
+declaration. Please note, the Function::Parameters pragma will be loaded using
+the defaults (i.e. not in strict-mode).
 
 =cut
 
